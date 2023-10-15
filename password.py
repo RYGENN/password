279220cc48bfb103396c432285c0ea5935e93609
Anupam Mahato <91363907+RYGENN@users.noreply.github.com>
2023-10-15 23:19:25 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 import random
 
-chars="qwertyuiopasdfghjklzxcvbnm0123456789ASDFGHJKLZXCVBNMQWERTYUIOP@$%&+=!"
+chars="qwertyuiopasdfghjklzxcvbnm0123456789ASDFGHJKLZXCVBNMQWERTYUIOP@$%&+=!?"
 length=int(input("Enter the length of the password:"))
 password=" "
 for a in range(length):
