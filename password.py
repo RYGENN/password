b59b660508f1e25967e127c15c678d6c8c2e208a
Anupam Mahato <91363907+RYGENN@users.noreply.github.com>
2023-12-19 23:41:02 +0530

    Update password.py

--- password.py
+++ password.py
@@ -3,5 +3,5 @@ chars="qwertyuiopasdfghjklzxcvbnm0123456789ASDFGHJKLZXCVBNMQWERTYUIOP@$%&+=!?"
 length=int(input("Enter the length of the password:"))
 password=" "
 for a in range(length):
-    password+=random.choice(chars)
+    password+=random.choice(chars) // disk app 
 print(password)
